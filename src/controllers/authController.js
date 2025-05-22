7de9613df788d49abee87340c48264882ecc7839
Vivek <vivekkalp@gmail.com>
2025-05-22 14:29:20 +0530

    part 6

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "../models/User"
+import User from "../models/User.js"
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
