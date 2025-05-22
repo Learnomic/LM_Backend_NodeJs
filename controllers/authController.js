1b7ff9489c34c200c27c8fd36937988aa5ab4db4
Vivek <vivekkalp@gmail.com>
2025-05-22 13:52:43 +0530

    hgjk

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // controllers/authController.js
-import User  from "../models/User";
+import User  from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
